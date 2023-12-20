3fb31f278ab877b913f8297c7f283f4d7da51ce1
milkfalling <hen85618@gmail.com>
2023-12-20 13:19:11 +0800

    Fix bug.

--- YTConverter.py
+++ YTConverter.py
@@ -15,17 +15,17 @@ handler = WebhookHandler('91b10665f17ce5d97f1038e07a7b6d04')
 def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
-
+    app.logger.info("Request body: " + body)
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
-    print("Webhook request received:", body)  # 添加這個print語句，顯示Webhook請求的內容
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
     if 'youtube.com/watch?v=' in event.message.text:
         url = event.message.text
         try:
@@ -60,4 +60,5 @@ def convert_to_wav(file_path):
     return wav_path
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
